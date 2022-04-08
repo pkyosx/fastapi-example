ce5a4c87e15f68529001184157a9a092840d141a
Seth Wang <seth_wang@trendmicro.com>
2022-04-08 23:09:45 +0800

    add metrics

---
+++ app/util/metrics_util.py
@@ -0,0 +1,25 @@
+from prometheus_client import Counter
+from prometheus_client import Gauge
+from prometheus_client import Info
+from prometheus_client import Summary
+
+
+class Metrics(object):
+    # Instance info
+    app = Info(
+        name='app',
+        documentation='Service information')
+
+    # API Request metrics
+    app_api_latency_seconds = Summary(
+        name='app_api_latency_seconds',
+        documentation='API request time in seconds',
+        labelnames=["method", "alias", "status_code"])
+    app_api_connection_total = Gauge(
+        name='app_api_connection_total',
+        documentation='API concurrent connection count',
+        labelnames=[])
+
+
+def update_latest(version):
+    Metrics.app.info({"version": version})
